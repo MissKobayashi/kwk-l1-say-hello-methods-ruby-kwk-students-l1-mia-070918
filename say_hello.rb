7ebe4bc25ec06eadb82c6ac021613cac8afa0bbd
MissKobayashi <priscin2002@gmail.com>
2018-07-09 19:30:32 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,12 @@
-def
+# def
 
-say_hello("Gabriela")
+# say_hello("Gabriela")
 
-end
+# end
+def name
+puts "Hello what's your name?"
+name = gets.chomp
+puts "Nice to meet you #{name} what do you love?"
+end 
+
+name
